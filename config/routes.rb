86b7637f484fbb032c73ef519bf5400b08cf1de4
zack <48981988+zakaahmad1561@users.noreply.github.com>
2022-04-30 17:38:54 +0500

    first_name & last_name has been added successfully added in user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :movies
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, 
+  controllers: { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_111629) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_30_122042) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_111629) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
