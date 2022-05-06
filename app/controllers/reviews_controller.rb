3150bb6a02a83f19919aa8ed690f46cdabe23db5
zack <48981988+zakaahmad1561@users.noreply.github.com>
2022-05-06 14:50:26 +0500

    Comments added function is working perfectly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,18 @@ class ReviewsController < ApplicationController
 
     end
 
+    def update
+        @review = @movie.reviews.find(params[:id])
+    
+        respond_to do |format|
+          if @review.update(review_params)
+            format.html { redirect_to movie_url(@movie), notice: 'Comment has been updated' }
+          else
+            format.html { redirect_to movie_url(@movie), alert: 'Comment was not updated!' }
+          end
+        end
+    end
+
     def destroy
         @review = @movie.reviews.find(params[:id])
         @review.destroy
